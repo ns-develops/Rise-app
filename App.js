5e3a38a7d9d1b87edad488b295bb0fe2041d9c22
”ns-develops” <test.tt9061030@gmail.com>
2025-08-14 00:35:43 +0200

    added fontsome quotes

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
+import { FontAwesome5 } from '@expo/vector-icons';
 import React from 'react';
-import { View, Text, Touchable, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 
 const App = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#F0F0F0' }}> 
-      <View style={{ width: '90%', backgroundColor: '#fff', borderRadius: 20, padding: 20 }}>
+    <View 
+      style={{ 
+        flex: 1, 
+        justifyContent: 'center', 
+        alignItems: 'center', 
+        backgroundColor: '#F0F0F0' 
+      }}
+    > 
+      <View 
+        style={{ 
+          width: '90%', 
+          backgroundColor: '#fff', 
+          borderRadius: 20, 
+          padding: 20 
+        }}
+      >
         <Text
           style={{
             textAlign: 'center',
@@ -16,6 +31,14 @@ const App = () => {
         >
           Quote of the Day
         </Text>
+
+        <FontAwesome5 
+          name="quote-left" 
+          size={20} 
+          color="#000" 
+          style={{ textAlign: 'left', marginBottom: -12 }} 
+        />
+
         <Text
           style={{
             color: '#000',
@@ -26,11 +49,49 @@ const App = () => {
             textAlign: 'center',
             marginBottom: 10,
             paddingHorizontal: 30,
-          }}>
-            "Test text under the quote"
-            </Text>
-     
-            <Text>New Quote</Text>
+          }}
+        >
+          Test text under the quote
+        </Text>
+        <FontAwesome5 
+          name="quote-right" 
+          size={20} 
+          color="#000" 
+          style={{ textAlign: 'right', marginTop: -20, marginBottom:20 }} 
+        />
+
+        <Text 
+          style={{
+            textAlign: 'right', 
+            fontWeight: '300', 
+            fontStyle: 'italic',
+            fontSize: 16,
+            color: '#000',
+            marginBottom: 10
+          }}
+        >
+          - Author Name
+        </Text>
+
+        <TouchableOpacity 
+          onPress={() => {}} 
+          style={{
+            backgroundColor: '#5B9BFF',
+            padding: 20,
+            borderRadius: 30,
+            marginVertical: 20
+          }}
+        >
+          <Text 
+            style={{
+              color: '#fff', 
+              fontSize: 18, 
+              textAlign: 'center'
+            }}
+          >
+            New Quote
+          </Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
