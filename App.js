d3c711fe153c3d3e9d9d6c1555c2892ae81fa151
”ns-develops” <test.tt9061030@gmail.com>
2025-08-13 23:50:44 +0200

    Intial commit with backgroundcolor

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#F0F0F0' }}> 
+      <View style={{ width: '90%', backgroundColor: '#fff', borderRadius: 20, padding: 20 }}>
+        <Text
+          style={{
+            textAlign: 'center',
+            fontSize: 26,
+            fontWeight: '600',
+            color: '#333',
+            marginBottom: 20,
+          }}
+        >
+          Quote of the Day
+        </Text>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
