be235ce8e544d6e4abaca6daf863b07d4b54a784
”ns-develops” <test.tt9061030@gmail.com>
2025-11-30 18:40:31 +0100

    upd config

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function Page() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text style={{ fontSize: 64, fontWeight: "bold" }}>Rise</Text>
+    </View>
+  );
+}
